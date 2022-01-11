1bb8990c000c49a755055a46d0d33a0751ebb0be
zsarge <zackarywsargent+github@gmail.com>
2022-01-11 13:17:53 -0500

    ban student emails and add whitelist

--- auth0-scripts/domain-whitelist.js
+++ auth0-scripts/domain-whitelist.js
@@ -3,25 +3,28 @@
  * https://manage.auth0.com/dashboard/us/$SERVER_NAME/rules
  */
 
-"use strict";
-
 function emailDomainWhitelist(user, context, callback) {
+  "use strict";
 
   // Access should only be granted to verified users.
   if (!user.email || !user.email_verified) {
-	return callback(new UnauthorizedError('Access denied.'));
+	return callback(new UnauthorizedError("Access denied. Email either doesn't exist or was not verified."));
   }
 
   const [username, mailserver] = user.email.split("@");
   const usernameRegex = /^(e\d\d-|eld-).*$/gm;
-  const validUsername = usernameRegex.test(username);
+  const validUsername = !usernameRegex.test(username);
   const validServer   = mailserver === "elderhs.net";
   const userHasAccess = validUsername && validServer;
 
-  if (!userHasAccess) {
+  const whitelist = ["e22-sargentzw@elderhs.net"];
+  const userInWhitelist = whitelist.some(email => email === user.email);
+
+  if (!userHasAccess && !userInWhitelist) {
 	return callback(new UnauthorizedError('Access denied. Your email was not recognized as a teacher.'));
   }
 
   return callback(null, user, context);
 }
+
 
