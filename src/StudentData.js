6b95cd1e487ea2e57dd5614aa0758b554697dde2
zsarge <zackarywsargent+github@gmail.com>
2022-01-19 10:35:53 -0500

    start getting students by their ID

---
+++ src/StudentData.js
@@ -0,0 +1,18 @@
+
+/**
+ * Full student information
+ * @typedef {{studentId: string, lName: string, fName: string, mName: String, email: string, gradYear: string}} Student
+ */
+
+/**
+ * getStudent takes a student Id and returns an object with
+ * all of the student data.
+ *
+ * @param {string} studentId - The student id
+ * @return {Student} - the {@link Student} with that ID
+ * @throws {StudentNotFoundException} - Student was not found 
+ */
+function getStudent(studentId) {
+	// ...
+}
+
