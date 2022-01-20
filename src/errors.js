69a36cff64bd8e486fde2513a2ff61d7f7b9f69d
zsarge <zackarywsargent@gmail.com>
2022-01-19 19:11:27 -0500

    add more jsdoc info

--- src/errors.js
+++ src/errors.js
@@ -3,8 +3,14 @@
 /**
  * A StudentNotFoundError should be thrown if a student cannot be found in
  * our records. This should be properly handled by the caller.
+ * @class
+ * @augments Error
  */
 class StudentNotFoundError extends Error {
+	/**
+	 * Creates a new StudentNotFoundError
+	 * @param {string} studentId - The student id to format
+	 */
 	constructor(studentId) {
 		super(`Error! Could not find student "${studentId}".`);
 		this.name = "StudentNotFoundError";
@@ -13,8 +19,15 @@ class StudentNotFoundError extends Error {
 
 /**
  * A FileNotFoundError should be thrown if a file could not be found.
+ * @class
+ * @augments Error
  */
 class FileNotFoundError extends Error {
+	/**
+	 * Creates a new FileNotFoundError
+	 * @param {string} message - The message to the user
+	 * @param {string} filePath - The place where the file should have been
+	 */
 	constructor(message, filePath) {
 		super(`${message} @ ${filePath}`);
 		this.name = "FileNotFoundError";
@@ -25,5 +38,4 @@ module.exports = {
 	StudentNotFoundError,
 	FileNotFoundError
 }
-
 
