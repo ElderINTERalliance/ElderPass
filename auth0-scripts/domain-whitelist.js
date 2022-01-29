095ac4fa9b25b97c32d753214f5a1fb096146e2b
zsarge <zackarywsargent@gmail.com>
2022-01-28 19:47:27 -0500

    format rule

--- auth0-scripts/domain-whitelist.js
+++ auth0-scripts/domain-whitelist.js
@@ -8,20 +8,20 @@ function emailDomainWhitelist(user, context, callback) {
 
   // Access should only be granted to verified users.
   if (!user.email || !user.email_verified) {
-	return callback(new UnauthorizedError("Access denied. Email either doesn't exist or was not verified."));
+    return callback(new UnauthorizedError("Access denied. Email either doesn't exist or was not verified."));
   }
 
   const [username, mailserver] = user.email.split("@");
   const usernameRegex = /^(e\d\d-|eld-).*$/gm;
   const validUsername = !usernameRegex.test(username);
-  const validServer   = mailserver === "elderhs.net";
+  const validServer = mailserver === "elderhs.net";
   const userHasAccess = validUsername && validServer;
 
   const whitelist = ["e22-sargentzw@elderhs.net"];
   const userInWhitelist = whitelist.some(email => email === user.email);
 
   if (!userHasAccess && !userInWhitelist) {
-	return callback(new UnauthorizedError('Access denied. Your email was not recognized as a teacher.'));
+    return callback(new UnauthorizedError('Access denied. Your email was not recognized as a teacher.'));
   }
 
   return callback(null, user, context);
